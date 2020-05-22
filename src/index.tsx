829fcc19f65d91b234690c0ffb525d56c615efc3
Ivan Osypov <ozzysk8cv@gmail.com>
2020-05-22 14:34:07 +0300

    Added radio to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-export { default as Tree } from "./components/Tree";
-
 export { default as List } from "./components/List";
 
 export { default as Menu } from "./components/Menu";
 
+export { default as Tree } from "./components/Tree";
+
 export { default as Input } from "./components/Input";
+
+export { default as Radio } from "./components/Radio";
 
 export { default as Button } from "./components/Button";
 
