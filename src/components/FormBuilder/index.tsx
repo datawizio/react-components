7c4ac44b4ef7aedaea4cfd08a69cde5fb0ae087e
Ivan Osypov <iosypov.dw@gmail.com>
2020-06-19 09:35:28 +0300

    Fix radio

--- src/components/FormBuilder/index.tsx
+++ src/components/FormBuilder/index.tsx
@@ -2,6 +2,7 @@ export { FieldText } from "./fields/Text";
 
 export { FieldPhone } from "./fields/Phone";
 export { FieldImage } from "./fields/Image";
+export { FieldRadio } from "./fields/Radio";
 export { FieldCheckbox } from "./fields/Checkbox";
 export { FieldPassword } from "./fields/Password";
 export { FieldDatePicker } from "./fields/DatePicker";
