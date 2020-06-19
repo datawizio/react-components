c488a15770679f2e5683971c8bfea6fb6d1ea526
Ivan Osypov <iosypov.dw@gmail.com>
2020-06-19 09:30:40 +0300

    Add radio field

---
+++ src/components/FormBuilder/fields/Radio.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from "react";
+
+import { Form } from "antd";
+import Radio from "../../Radio";
+
+import { FieldRadioProps } from "../types";
+import { RadioChangeEvent } from "antd/lib/radio";
+
+export const FieldText: React.FC<FieldRadioProps> = React.memo(
+  ({ label, rules, name, options, onChange }) => {
+    const handleFieldChange = useCallback(
+      ({ target: { value } }: RadioChangeEvent) => {
+        onChange({
+          name,
+          value
+        });
+      },
+      [onChange]
+    );
+
+    return (
+      <Form.Item name={name} label={label} rules={rules}>
+        <Radio.Group name={name} onChange={handleFieldChange}>
+          {options.map(option => (
+            <Radio value={option.value} key={option.value}>
+              {option.label}
+            </Radio>
+          ))}
+        </Radio.Group>
+      </Form.Item>
+    );
+  }
+);
