e32bb4379fa0012c9d02bd596373809ab4929aeb
Ivan Osypov <iosypov.dw@gmail.com>
2020-07-14 17:51:20 +0300

    Added field switch

---
+++ src/components/FormBuilder/fields/Switch.tsx
@@ -0,0 +1,30 @@
+import React, { useCallback } from "react";
+
+import { Form, Switch } from "antd";
+
+import { FieldCheckboxProps } from "../types";
+
+export const FieldSwitch: React.FC<FieldCheckboxProps> = React.memo(
+  ({ label, rules, name, onChange }) => {
+    const handleFieldChange = useCallback(
+      (checked: boolean) => {
+        onChange({
+          name,
+          value: checked
+        });
+      },
+      [onChange, name]
+    );
+
+    return (
+      <Form.Item
+        name={name}
+        label={label}
+        rules={rules}
+        valuePropName="checked"
+      >
+        <Switch onChange={handleFieldChange} />
+      </Form.Item>
+    );
+  }
+);
--- src/components/FormBuilder/index.tsx
+++ src/components/FormBuilder/index.tsx
@@ -3,6 +3,7 @@ export { FieldText } from "./fields/Text";
 export { FieldPhone } from "./fields/Phone";
 export { FieldImage } from "./fields/Image";
 export { FieldRadio } from "./fields/Radio";
+export { FieldSwitch } from "./fields/Switch";
 export { FieldCheckbox } from "./fields/Checkbox";
 export { FieldPassword } from "./fields/Password";
 export { FieldDatePicker } from "./fields/DatePicker";
