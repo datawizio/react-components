c0cfa0bbd798a21e538a7bca1dbd7225d7482402
Ivan Osypov <iosypov.dw@gmail.com>
2020-08-12 15:52:19 +0300

    fix drawer

--- src/components/DrawerTreeSelect/index.tsx
+++ src/components/DrawerTreeSelect/index.tsx
@@ -388,7 +388,7 @@ const DrawerTreeSelect: FCDrawerTreeSelect<SelectValue> = ({
   const handlerDrawerCancel = useCallback(() => {
     const prevValue = !multiple && !value ? [] : value;
     //@ts-ignore
-    if (prevValue.length === 0) showAllRef.current = true;
+    if (prevValue && prevValue.length === 0) showAllRef.current = true;
     dispatch({
       type: "setState",
       payload: {
