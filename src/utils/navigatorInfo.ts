c43a22710b10908d743548af27fc36cffa38f1a3
Ivan Osypov <iosypov.dw@gmail.com>
2020-08-28 14:20:14 +0300

    Add navigator info

---
+++ src/utils/navigatorInfo.ts
@@ -0,0 +1,40 @@
+export function getBrowserName() {
+  var regexps: any = {
+      "Chrome": [/Chrome\/(\S+)/],
+      "Firefox": [/Firefox\/(\S+)/],
+      "MSIE": [/MSIE (\S+);/],
+      "Opera": [
+        /Opera\/.*?Version\/(\S+)/ /* Opera 10 */,
+        /Opera\/(\S+)/ /* Opera 9 and older */
+      ],
+      "Safari": [/Version\/(\S+).*?Safari\//]
+    },
+    re,
+    m,
+    browser,
+    version;
+
+  const userAgent = window.navigator.userAgent;
+  const elements = 1;
+  for (browser in regexps) {
+    while ((re = regexps[browser].shift()))
+      if ((m = userAgent.match(re))) {
+        //@ts-ignore
+        version = m[1].match(
+          new RegExp("[^.]+(?:.[^.]+){0," + elements + "}")
+        )[0];
+        return browser + " " + version;
+      }
+  }
+  return "unknown";
+}
+
+export function getGeoLocation(callback: (pos: any) => void) {
+  navigator.geolocation &&
+    navigator.geolocation.getCurrentPosition(pos => {
+      callback({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude
+      });
+    });
+}
