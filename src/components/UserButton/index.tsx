a642fb03c7273635cf8bd4751f4be9bfbe6926c0
Ivan Osypov <iosypov.dw@gmail.com>
2020-09-04 16:06:18 +0300

    UserButton with out menu

--- src/components/UserButton/index.tsx
+++ src/components/UserButton/index.tsx
@@ -20,16 +20,22 @@ const UserButton: React.FC<UserButtonProps> = ({
   menu,
   showFullName
 }) => {
-  return (
+  const button = (
+    <Button border={false} className="user-name">
+      <UserAvatar src={photo} name={fullName} />
+      {showFullName ? (
+        <span className="user-button-fullname">{fullName}</span>
+      ) : null}
+      <DownOutlined />
+    </Button>
+  );
+
+  return menu ? (
     <Dropdown overlay={menu} className="user-dropdown" trigger={["click"]}>
-      <Button border={false} className="user-name">
-        <UserAvatar src={photo} name={fullName} />
-        {showFullName ? (
-          <span className="user-button-fullname">{fullName}</span>
-        ) : null}
-        <DownOutlined />
-      </Button>
+      {button}
     </Dropdown>
+  ) : (
+    button
   );
 };
 
