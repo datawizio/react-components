1a52ab0ff19189b59446e06bfa41c75d071739c8
Mac <mac@Macs-iMac.local>
2020-10-07 00:35:25 -0700

    refactor

--- src/utils/sizeUtils.ts
+++ src/utils/sizeUtils.ts
@@ -1,16 +1,16 @@
 export function getAbsoluteHeight(el: HTMLElement) {
-  var styles = window.getComputedStyle(el);
+  const styles = window.getComputedStyle(el);
 
-  var margin =
+  const margin =
     parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
 
   return Math.ceil(el.offsetHeight + margin);
 }
 
 export function getAbsoluteWidth(el: HTMLElement) {
-  var styles = window.getComputedStyle(el);
+  const styles = window.getComputedStyle(el);
 
-  var margin =
+  const margin =
     parseFloat(styles["marginLeft"]) + parseFloat(styles["marginRight"]);
 
   return Math.ceil(el.offsetWidth + margin);
