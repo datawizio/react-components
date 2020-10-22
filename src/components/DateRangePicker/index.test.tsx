0b55912796515c4146665ec3ae2bc7c94d655983
yura <abramovich.cp@gmail.com>
2020-10-22 17:16:00 +0300

    DrawerRangePicker

---
+++ src/components/DateRangePicker/index.test.tsx
@@ -0,0 +1,54 @@
+import "jsdom-global/register";
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+jest.mock("./presets", () => ({
+  DefaultPreset: {
+    yesterday: "2020-10-22T13:06:36.873Z",
+    lastWeek: "2020-10-22T13:06:36.873Z",
+    currentMonth: "2020-10-22T13:06:36.873Z",
+    last_30_days: "2020-10-22T13:06:36.873Z",
+    last_90_days: "2020-10-22T13:06:36.873Z",
+    currentYear: "2020-10-22T13:06:36.873Z"
+  }
+}));
+
+import DateRangePicker from "./index";
+
+const mockProps = {
+  defaultPresetUsed: true,
+  fullWidth: true,
+  maxDate: "2020-10-22T13:06:36.873Z",
+  onChange: jest.fn()
+};
+
+const setUp = (props?) => mount(<DateRangePicker {...props} />);
+
+describe(" DateRangePicker component", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp(mockProps);
+  });
+
+  it("DateRangePicker rendered correctly", () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it("DateRangePicker rendered correctly with empty props", () => {
+    const wrapper = setUp();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("DateRangePicker trigger onChange with empty", () => {
+    const rangePicker = component.find("RangePicker").first();
+    rangePicker.props().onChange("");
+    expect(component).toMatchSnapshot();
+  });
+
+  it("DateRangePicker trigger onChange with mock", () => {
+    const dateRange = ["2020-10-22T13:06:36.873Z", "2020-11-22T13:06:36.873Z"];
+    const rangePicker = component.find("RangePicker").first();
+    rangePicker.props().onChange(dateRange);
+    expect(component).toMatchSnapshot();
+  });
+});
