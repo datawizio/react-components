6e8639a438db045b8a770ed729bbc7d90f8132c3
nick <nickelaos92@gmail.com>
2020-10-30 15:59:28 +0200

    Fix styles

--- src/components/Help/components/Menu/index.tsx
+++ src/components/Help/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useContext, useMemo } from "react";
-import { Menu, Dropdown } from "antd";
+import { Menu, Dropdown, Button } from "antd";
 import { QuestionCircleOutlined } from "@ant-design/icons";
 import ConfigContext from "../../../ConfigProvider/context";
 import "./index.less";
@@ -16,10 +16,10 @@ const HelpMenu: React.FC<IHelpMenu> = ({ onTutorialClick, onSupportClick }) => {
   const menu = useMemo(() => {
     return (
       <Menu theme="light" className="help-menu-dropdown">
-        <Menu.Item key="1" onClick={onTutorialClick} style={{padding: "2px 12px"}}>
+        <Menu.Item key="1" onClick={onTutorialClick}>
           {translate("READ_TUTORIAL")}
         </Menu.Item>
-        <Menu.Item key="2" onClick={onSupportClick} style={{padding: "2px 12px"}}>
+        <Menu.Item key="2" onClick={onSupportClick}>
           {translate("SUPPORT")}
         </Menu.Item>
       </Menu>
@@ -29,9 +29,9 @@ const HelpMenu: React.FC<IHelpMenu> = ({ onTutorialClick, onSupportClick }) => {
   return (
     <>
       <Dropdown overlay={menu} trigger={["click"]} placement="bottomRight">
-        <a href="#" className="help-icon" onClick={e => e.preventDefault()}>
+        <Button type="link" className="help-icon" onClick={e => e.preventDefault()}>
           <QuestionCircleOutlined />
-        </a>
+        </Button>
       </Dropdown>
     </>
   );
