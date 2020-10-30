72fb97b9f5af0030450a9897209c17ff25fe6c71
yura <abramovich.cp@gmail.com>
2020-10-30 15:48:53 +0200

    mock window.document

--- setupTests.ts
+++ setupTests.ts
@@ -1,11 +1,14 @@
-import Enzyme, { shallow, render, mount } from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import toJson from "enzyme-to-json";
+import { JSDOM } from "jsdom";
+
+const dom = new JSDOM();
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
-// Fail tests on any warning
+global.document = dom.window.document;
+
 global.console.error = message => {
   throw new Error(message);
 };
