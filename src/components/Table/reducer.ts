70dd4122d0d0cdee01d1da0e5be7f9189175ab90
Ivan Osypov <iosypov.dw@gmail.com>
2020-11-26 14:48:50 +0200

    Make force columns update

--- src/components/Table/reducer.ts
+++ src/components/Table/reducer.ts
@@ -54,6 +54,7 @@ export function initializer(props: TableProps): TableState {
     dataSource,
     pagination,
     searchValue,
+    forceColumns,
     dTypesConfig,
     showSizeChanger,
     pageSizeOptions,
@@ -65,6 +66,8 @@ export function initializer(props: TableProps): TableState {
     loading,
     dataSource,
     searchValue,
+
+    forceColumns,
 
     pagination: {
       showSizeChanger,
@@ -107,6 +110,9 @@ export function reducer(state: TableState, action: Action): TableState {
         state.visibleColumnsKeys.filter(key => nextColumnsMap[key]);
 
       const nextColumns = (function rec(newColumns, oldColumns) {
+        if (state.forceColumns) {
+          return newColumns;
+        }
         const sameDataIndex = newColumn => oldColumn =>
           newColumn.dataIndex === oldColumn.dataIndex;
 
