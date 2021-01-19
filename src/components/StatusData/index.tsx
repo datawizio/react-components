e25a0e0bc28a16f39c6053c966e109dbf0628f42
nick <nickelaos92@gmail.com>
2021-01-19 12:50:43 +0200

    Code refactoring.

--- src/components/StatusData/index.tsx
+++ src/components/StatusData/index.tsx
@@ -1,7 +1,6 @@
-import React, { useContext, useMemo } from "react";
-import dayjs from "dayjs";
+import React, { useContext } from "react";
 import ConfigContext from "../ConfigProvider/context";
-import { capitalize } from "../../utils/text";
+import { formatDateTime } from "../../utils/date";
 import "./index.less";
 
 interface StatusDataProps {
@@ -15,52 +14,23 @@ const StatusData: React.FC<StatusDataProps> = ({
 }) => {
   const { translate } = useContext(ConfigContext);
 
-  return useMemo(() => {
-    if (inProcess) {
-      return (
-        <div className="StatusData__container">
-          <p>{translate("DATA_IN_PROGRESS")}</p>
-        </div>
-      );
-    }
+  if (inProcess) {
+    return (
+      <div className="StatusData__container">
+        <p>{translate("DATA_IN_PROGRESS")}</p>
+      </div>
+    );
+  }
 
-    if (lastUpdateDate) {
-      let output;
-
-      const dateFormat = "DD.MM.YYYY";
-      const timeFormat = "HH:mm";
-
-      const today = dayjs().format(dateFormat);
-      const yesterday = dayjs().subtract(1, "day").format(dateFormat);
-
-      const date = dayjs(lastUpdateDate).format(dateFormat);
-      const time = dayjs(lastUpdateDate).format(timeFormat);
-
-      switch (date) {
-        case today:
-          output = `${capitalize(translate("TODAY"))} ${translate(
-            "AT"
-          )} ${time}`;
-          break;
-        case yesterday:
-          output = `${capitalize(translate("YESTERDAY"))} ${translate(
-            "AT"
-          )} ${time}`;
-          break;
-        default:
-          output = `${date} ${time}`;
-      }
-
-      return (
-        <div className="StatusData__container">
-          <p>{output}</p>
-          <p>{translate("LAST_UPDATE_DATA")}</p>
-        </div>
-      );
-    }
-
-    return null;
-  }, [translate, inProcess, lastUpdateDate]);
+  if (lastUpdateDate) {
+    const formattedDateTime = formatDateTime(lastUpdateDate, translate);
+    return (
+      <div className="StatusData__container">
+        <p>{formattedDateTime}</p>
+        <p>{translate("LAST_UPDATE_DATA")}</p>
+      </div>
+    );
+  }
 };
 
 export default StatusData;
