e505c46703a8a8631f7bce323b0407e86d501689
nick <nickelaos92@gmail.com>
2021-04-19 12:55:43 +0300

    Add new presets for DateRangePicker. Code refactoring.

--- src/components/DateRangePicker/presets.ts
+++ src/components/DateRangePicker/presets.ts
@@ -1,37 +1,141 @@
 import dayjs from "dayjs";
-import { DateRange, DefaultPresetType } from "./types";
+import quarterOfYear from "dayjs/plugin/quarterOfYear";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { DateRange, DefaultPresetPrevType, DefaultPresetType } from "./types";
+import { genPrevPeriod } from "./utils";
+
+dayjs.extend(quarterOfYear);
+dayjs.extend(customParseFormat);
+
+const format = "DD-MM-YYYY";
 
 export const DefaultPresetRanges: DefaultPresetType = {
-  get yesterday(): DateRange {
-    return [dayjs().subtract(1, "d"), dayjs().subtract(1, "d")];
+  yesterday: (maxDate = null): DateRange => {
+    const max = maxDate
+      ? dayjs(maxDate, format).subtract(1, "d")
+      : dayjs().subtract(1, "d");
+    return [max, max];
   },
 
-  get lastWeek(): DateRange {
-    return [dayjs().subtract(6, "d"), dayjs()];
+  lastWeek: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).subtract(6, "d")
+      : dayjs().subtract(6, "d");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
   },
 
-  get currentMonth(): DateRange {
-    return [dayjs().startOf("month"), dayjs()];
+  currentMonth: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).startOf("month")
+      : dayjs().startOf("month");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
   },
 
-  get last_30_days(): DateRange {
-    return [dayjs().subtract(29, "d"), dayjs()];
+  last_30_days: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).subtract(29, "d")
+      : dayjs().subtract(29, "d");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
   },
 
-  get last_90_days(): DateRange {
-    return [dayjs().subtract(89, "d"), dayjs()];
+  last_90_days: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).subtract(89, "d")
+      : dayjs().subtract(89, "d");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
   },
 
-  get currentYear(): DateRange {
-    return [dayjs().startOf("y"), dayjs()];
+  quarterBegin: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).startOf("quarter")
+      : dayjs().startOf("quarter");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
+  },
+
+  last_180_days: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).subtract(179, "d")
+      : dayjs().subtract(179, "d");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
+  },
+
+  last_365_days: (maxDate = null): DateRange => {
+    const min = maxDate
+      ? dayjs(maxDate, format).subtract(1, "year")
+      : dayjs().subtract(1, "year");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
+  },
+
+  currentYear: (maxDate = null): DateRange => {
+    const min = maxDate ? dayjs(maxDate, format).startOf("y") : dayjs().startOf("y");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
+  },
+
+  allPeriod: (minDate = null, maxDate = null): DateRange => {
+    const min = minDate ? dayjs(minDate, format) : dayjs().subtract(10, "year");
+    const max = maxDate ? dayjs(maxDate, format) : dayjs();
+    return [min, max];
   }
 };
 
-export const DefaultPreset = {
-  "Yesterday": DefaultPresetRanges.yesterday,
-  "Last_week": DefaultPresetRanges.lastWeek,
-  "Current_month": DefaultPresetRanges.currentMonth,
-  "Last_30_Days": DefaultPresetRanges.last_30_days,
-  "Last_90_Days": DefaultPresetRanges.last_90_days,
-  "cur_year": DefaultPresetRanges.currentYear,
-}
+export const DefaultPresetPrevRanges: DefaultPresetPrevType = {
+  previous: (dateFrom = null, dateTo = null): DateRange => {
+    let [min, max] = genPrevPeriod(dateFrom, dateTo);
+    min = min ? dayjs(min, format) : null;
+    max = max ? dayjs(max, format) : null;
+    return [min, max];
+  },
+  prev_last_week: (dateFrom = null, dateTo = null): DateRange => {
+    const min = dayjs(dateFrom, format).subtract(1, "week");
+    const max = dayjs(dateTo, format).subtract(1, "week");
+    return [min, max];
+  },
+  prev_last_month: (dateFrom = null, dateTo = null): DateRange => {
+    const min = dayjs(dateFrom, format).subtract(1, "month");
+    const max = dayjs(dateTo, format).subtract(1, "month");
+    return [min, max];
+  },
+  prev_last_quarter: (dateFrom = null, dateTo = null): DateRange => {
+    const min = dayjs(dateFrom, format).subtract(1, "quarter");
+    const max = dayjs(dateTo, format).subtract(1, "quarter");
+    return [min, max];
+  },
+  prev_last_year: (dateFrom = null, dateTo = null): DateRange => {
+    const min = dayjs(dateFrom, format).subtract(1, "year");
+    const max = dayjs(dateTo, format).subtract(1, "year");
+    return [min, max];
+  }
+};
+
+export const DefaultPreset = (minDate, maxDate) => {
+  return {
+    "Yesterday": DefaultPresetRanges.yesterday(maxDate),
+    "Last_week": DefaultPresetRanges.lastWeek(maxDate),
+    "Current_month": DefaultPresetRanges.currentMonth(maxDate),
+    "Last_30_Days": DefaultPresetRanges.last_30_days(maxDate),
+    "Last_90_Days": DefaultPresetRanges.last_90_days(maxDate),
+    "LAST_180_DAYS": DefaultPresetRanges.last_180_days(maxDate),
+    "SEASON_BEGIN": DefaultPresetRanges.quarterBegin(maxDate),
+    "cur_year": DefaultPresetRanges.currentYear(maxDate),
+    "LAST_365_DAYS": DefaultPresetRanges.last_365_days(maxDate),
+    "All_period": DefaultPresetRanges.allPeriod(minDate, maxDate)
+  };
+};
+
+export const DefaultPresetPrev = (dateFrom, dateTo) => {
+  return {
+    "PREVIOUS": DefaultPresetPrevRanges.previous(dateFrom, dateTo),
+    "PREV_LAST_WEEK": DefaultPresetPrevRanges.prev_last_week(dateFrom, dateTo),
+    "PREV_LAST_MONTH": DefaultPresetPrevRanges.prev_last_month(dateFrom, dateTo),
+    "PREV_LAST_QUARTER": DefaultPresetPrevRanges.prev_last_quarter(dateFrom, dateTo),
+    "PREV_LAST_YEAR": DefaultPresetPrevRanges.prev_last_year(dateFrom, dateTo),
+  };
+};
