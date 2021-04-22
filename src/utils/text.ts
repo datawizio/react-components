10ec2ec22d22858d4552a1b2267d0cd72bb8a149
nickelaos <nickelaos92@gmail.com>
2021-04-22 14:08:54 +0300

    Fix table styles on mobile. Add copyToClipboard() function.

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -15,3 +15,22 @@ export function parseErrorText(errors: any) {
 export const capitalize = (s: string) => {
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
+
+export function copyToClipboard(textToCopy) {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(textToCopy);
+  } else {
+    const textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise((res, rej) => {
+      document.execCommand("copy") ? res() : rej();
+      textArea.remove();
+    });
+  }
+}
