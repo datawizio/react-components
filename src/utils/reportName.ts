1fea580400c93f897684789c5c22c18a6e019374
Ivan <ivanderda1999@gmail.com>
2021-04-26 15:21:08 +0300

    fix problem

--- src/utils/reportName.ts
+++ src/utils/reportName.ts
@@ -6,7 +6,7 @@ export const getReportName = (
 
   const matchedData = pathname.match(/\/c\/\d+\/(.*)/i);
   res = matchedData && matchedData.length === 2 ? matchedData[1] : pathname;
-  res = res.split("/")[0];
-  res = res.length === 3 ? `${res[0]}_${res[2]}` : res[0];
+  let result: string[] = res.split("/");
+  res = result.length === 3 ? `${result[0]}_${result[2]}` : result[0];
   return res || defaultReportName
 };
