c3570193e35cff7c965ac28bc173ab5c56bccdad
Ivan <ivanderda1999@gmail.com>
2021-04-26 15:14:16 +0300

    update get report name from url

--- src/utils/reportName.ts
+++ src/utils/reportName.ts
@@ -7,6 +7,6 @@ export const getReportName = (
   const matchedData = pathname.match(/\/c\/\d+\/(.*)/i);
   res = matchedData && matchedData.length === 2 ? matchedData[1] : pathname;
   res = res.split("/")[0];
-
+  res = res.length === 3 ? `${res[0]}_${res[2]}` : res[0];
   return res || defaultReportName
 };
