ecd4927949ab007d2138101579dcd310129ce560
Ivan Osypov <iosypov.dw@gmail.com>
2021-06-04 12:06:08 +0300

    asd

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -4,6 +4,20 @@ import generatePicker from "antd/lib/date-picker/generatePicker";
 
 import "./index.less";
 
+// dayjsGenerateConfig.getEndDate = date => {
+//   return date.startOf("month");
+// };
+
+// dayjsGenerateConfig.getWeekDay = date => {
+//   const clone = date.locale("en");
+//   //@ts-ignore
+//   return clone.weekday() + clone.localeData().firstDayOfWeek() - 1;
+// };
+
+dayjsGenerateConfig.getMonth = date => {
+  return date.date() >= 30 ? date.month() + 1 : date.month();
+};
+
 const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
 export default DatePicker;
