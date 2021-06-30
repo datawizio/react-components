5f1cbe0a13f1ac382121e344024f7e2da41761c5
Ivan <ivanderda1999@gmail.com>
2021-06-30 17:05:18 +0300

    PeriodSelect test

---
+++ src/components/PeriodSelect/usePeriodSelect.test.tsx
@@ -0,0 +1,92 @@
+import "jsdom-global/register";
+import React from "react";
+import { usePeriodSelect } from "./usePeriodSelect";
+import { act } from "react-dom/test-utils";
+import { mount } from "enzyme";
+
+const updatePeriodAC = payload => ({
+  type: "updatePeriod",
+  payload
+});
+const updatePrevPeriodAC = payload => ({
+  type: "updatePrevPeriod",
+  payload
+});
+const updateDatePickerAC = payload => ({
+  type: "updateDatePicker",
+  payload
+});
+const updatePrevDatePickerAC = payload => ({
+  type: "updatePrevDatePicker",
+  payload
+});
+const clearPickerAC = payload => ({
+  type: "clearPicker",
+  payload
+});
+const clearPrevPickerAC = payload => ({
+  type: "clearPrevPicker",
+  payload
+});
+const setStateAC = payload => ({
+  type: "setState",
+  payload
+});
+
+const initialStoreMockData = {
+  selectedPeriod: "last_update_date",
+  selectedPrevPeriod: "previous",
+  period: {
+    startDate: "2020-10-22",
+    endDate: "2020-11-22"
+  },
+  prevPeriod: {
+    startDate: "2020-09-20",
+    endDate: "2020-10-21"
+  },
+  showPeriodPicker: true,
+  showPrevPeriodPicker: true,
+  isPickerEmpty: false,
+  isPrevPickerEmpty: false,
+  availablePrevPeriods: "any",
+  clientDate: "2021-11-28",
+  clientStartDate: "2020-10-21"
+};
+
+function setUp(initialStore, action) {
+  let newStore;
+  function TestComponent() {
+    const reducerResult = usePeriodSelect(initialStore);
+    const [store, dispatch] = reducerResult;
+    const callAction = () => {
+      act(() => {
+        dispatch(action);
+      });
+    };
+    newStore = store;
+    return <button onClick={callAction} data-store={store}></button>;
+  }
+
+  const wrapper = mount(<TestComponent />);
+  wrapper.update();
+
+  return {
+    wrapper,
+    newStore
+  };
+}
+
+describe("Period select reducer", () => {
+  it("setState action", () => {
+    const propStoreKey = "value";
+    const payload = {
+      [propStoreKey]: "nextValue"
+    };
+
+    const { wrapper } = setUp(initialStoreMockData, setStateAC(payload));
+    wrapper.find("button").simulate("click");
+    const nextStore = wrapper.find("button").prop("data-store") as any;
+
+    expect(nextStore[propStoreKey]).toBe(payload[propStoreKey]);
+  });
+});
