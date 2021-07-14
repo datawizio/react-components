431fd39fd03c89fbb3532fe095537e3034463ad5
Ivan <ivanderda1999@gmail.com>
2021-07-14 18:01:57 +0300

    tableXLSXexporter test

--- src/components/TableXlsxExporter/index.test.tsx
+++ src/components/TableXlsxExporter/index.test.tsx
@@ -1,70 +1,141 @@
 import "jsdom-global/register";
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
-import { waitFor } from "@testing-library/react";
+import {
+  waitFor,
+  render,
+  fireEvent,
+  screen,
+  cleanup,
+  getByLabelText
+} from "@testing-library/react";
 import TableXlsxExporter from "./index";
 import Table from "../Table";
 import {
   getStaticColumn,
   getStaticDataSource,
-  getStatickModel,
   sortable
 } from "../Table/__mocks__";
 import { exportTableToXLSX } from "./exporters";
-import { TableProps } from "../Table/types";
+import { Menu } from "antd";
 
-const mockTableProps: TableProps = {
+const mockTableProps = {
   height: "auto",
   width: "auto",
   dTypesConfig: {},
+
   dataProvider: jest.fn(() => {
     return {
-      columns: [],
-      dataSource: []
+      columns: [
+        {
+          title: "Name",
+          key: "name",
+          dataIndex: "name"
+        }
+      ],
+      dataSource: [
+        {
+          key: "1",
+          name: "John"
+        }
+      ]
     };
   })
 };
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }))
+});
 
 const setUp = (props?) => mount(<TableXlsxExporter {...props} />);
 
 describe("TableXlsxExporter component", () => {
   let component;
-  let realUseContext;
-  let useContextMock;
+
   beforeEach(() => {
     component = setUp();
-    realUseContext = React.useContext;
-    useContextMock = React.useContext = jest.fn();
-  });
-
-  afterEach(() => {
-    React.useContext = realUseContext;
   });
 
   it("should render corectly", () => {
     expect(component).toMatchSnapshot();
   });
 
-  it("should loading notice render corectly", () => {
-    useContextMock.mockReturnValue({ ...mockTableProps });
-    const wrapper = shallow(
+  it("should render in Table ToolBar corectly", () => {
+    const { container } = render(
+      //@ts-ignore
       <Table {...mockTableProps}>
         <Table.ToolBar>
           <TableXlsxExporter exportHandler={exportTableToXLSX} />
         </Table.ToolBar>
       </Table>
     );
-    wrapper.render();
+    expect(container).toMatchSnapshot();
+  });
 
-    const XlsxExporterButton = wrapper.find("TableXlsxExporter").dive();
+  it("should export corectly", async () => {
+    const mockProps = {
+      height: "auto",
+      width: "auto",
 
-    XlsxExporterButton.find("Button").first().simulate("click");
+      sortable,
+      columns: getStaticColumn(),
+      dataSource: getStaticDataSource()
+    };
 
-    wrapper.update();
+    const wrapper = render(
+      //@ts-ignore
+      <Table {...mockProps}>
+        <Table.ToolBar>
+          <TableXlsxExporter exportHandler={exportTableToXLSX} />
+        </Table.ToolBar>
+      </Table>
+    );
+    const saveButton = wrapper.getByTitle("SAVE_BTN_TITLE");
+    act(() => {
+      fireEvent.click(saveButton);
+    });
 
     expect(
       document.querySelectorAll(".ant-message-notice")[0].textContent
     ).toBe("LOADING");
+  });
+
+  it("should TableXlsxExporter with menu render corectly", () => {
+    const mockProps = {
+      height: "auto",
+      width: "auto",
+
+      sortable,
+      columns: getStaticColumn(),
+      dataSource: getStaticDataSource()
+    };
+
+    const menuItems = () => {
+      return [<Menu.Item key="exporter">"SAVE_XLS"</Menu.Item>];
+    };
+
+    const wrapper = mount(
+      // @ts-ignore
+      <Table {...mockProps}>
+        <Table.ToolBar>
+          <TableXlsxExporter
+            exportHandler={exportTableToXLSX}
+            menuItems={menuItems}
+          />
+        </Table.ToolBar>
+      </Table>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
