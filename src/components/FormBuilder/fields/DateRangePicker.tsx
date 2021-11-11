677e0813da2b6e87a71ff6a6bc3c632ebd43b3e3
nick <nickelaos92@gmail.com>
2021-11-11 12:32:29 +0200

    Add 'allowClear' prop to FieldDateRangePickerProps.

--- src/components/FormBuilder/fields/DateRangePicker.tsx
+++ src/components/FormBuilder/fields/DateRangePicker.tsx
@@ -14,6 +14,7 @@ export interface DateRangePickerParams {
 
 export interface FieldDateRangePickerProps
   extends FormFieldProps<DateRangePickerParams> {
+  allowClear?: boolean;
   format?: string;
   storeFormat?: string;
   maxDate?: string;
