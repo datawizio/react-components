d267042b61a0c8949e18a6d5e2c52688ed467029
Ivan <ivanderda1999@gmail.com>
2021-11-30 17:01:23 +0200

    fix reset autoselect template, reset valide input

--- src/components/TableTemplates/components/Dropdown.tsx
+++ src/components/TableTemplates/components/Dropdown.tsx
@@ -1,18 +1,25 @@
 import * as React from "react";
-import { useCallback, useState, useContext, useMemo } from "react";
+import { useCallback, useState, useContext, useMemo, useEffect } from "react";
 import { SaveOutlined } from "@ant-design/icons";
 import clsx from "clsx";
 import Input from "../../Input";
 import ConfigContext from "../../ConfigProvider/context";
+import { useDeepEqualMemo } from "../../../hooks/useDeepEqualMemo";
 
 interface DropdownProps {
+  isOpen: boolean;
   onCreate: (title: string) => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ onCreate, children }) => {
+const Dropdown: React.FC<DropdownProps> = ({ isOpen, onCreate, children }) => {
   const { translate } = useContext(ConfigContext);
   const [inputValue, setInputValue] = useState<string>();
   const [isInputValueValid, setIsInputValueValid] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!isOpen) setIsInputValueValid(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [useDeepEqualMemo(isOpen)]);
 
   const handleChangeInput = useCallback(e => {
     setIsInputValueValid(Boolean(e.target.value?.trim()));
