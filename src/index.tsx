c24885b17bd1f30493b88614d276a854add429c0
Ivan <ivanderda1999@gmail.com>
2021-12-08 16:37:05 +0200

    remove export

--- src/index.tsx
+++ src/index.tsx
@@ -114,8 +114,6 @@ export { default as DateRangePicker } from "./components/DateRangePicker";
 
 export { default as DrawerTreeSelect } from "./components/DrawerTreeSelect";
 
-export { default as TableXlsxExporter } from "./components/TableXlsxExporter";
-
 export { default as TableTransposeButton } from "./components/TableTransposeButton";
 
 export { default as TableSelectColumnsModal } from "./components/TableSelectColumnsModal";
