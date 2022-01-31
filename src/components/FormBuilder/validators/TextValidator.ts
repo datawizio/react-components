1423094b5aefc08d95a8953d282fba0c16ced0bc
nick <nickelaos92@gmail.com>
2022-01-31 12:03:34 +0200

    Add default max value and message for maxLength method in TextValidator.

--- src/components/FormBuilder/validators/TextValidator.ts
+++ src/components/FormBuilder/validators/TextValidator.ts
@@ -1,3 +1,5 @@
+import i18n from "i18next";
+
 export const TextValidator = {
   required(params: any, value: string) {
     const { message } = params;
@@ -8,7 +10,13 @@ export const TextValidator = {
   },
 
   maxLength(params: any, value: string) {
-    const { message, max } = params;
+    const MAX_DEFAULT = 200;
+    let { message, max } = params;
+    if (!max) max = MAX_DEFAULT;
+    if (!message)
+      message = i18n.t("NAME_SHOULD_BE_LESS_THEN", {
+        maxCount: max
+      });
     if (value.length > max) {
       return Promise.reject(message);
     }
