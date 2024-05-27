98ab80b2dde83c790659ddcd89810378a779ff7e
nick <nickelaos92@gmail.com>
2024-05-27 18:03:48 +0300

    Add showApiNotifications() fn.

--- src/components/ApiError/index.tsx
+++ src/components/ApiError/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { notification } from "antd";
 
 export interface ApiErrorProps {
@@ -8,6 +7,12 @@ export interface ApiErrorProps {
 
 export interface FCApiError extends React.FC<ApiErrorProps> {
   showError: (errors: any) => void;
+  showNotification: (
+    message: string,
+    description?: string | null,
+    type?: "error" | "warning",
+    duration?: number
+  ) => void;
 }
 
 const ApiError: FCApiError = ({ errors }) => {
@@ -17,6 +22,28 @@ const ApiError: FCApiError = ({ errors }) => {
 ApiError.showError = (errors: any) => {
   notification.error({
     message: <ApiError errors={errors} />
+  });
+};
+
+ApiError.showNotification = (
+  message: string,
+  description?: string | null,
+  type?: "error" | "warning",
+  duration: number = 6
+) => {
+  const notificationFn =
+    type === "error" ? notification.error : notification.warning;
+  notificationFn({
+    message,
+    description: description ? (
+      <div dangerouslySetInnerHTML={{ __html: description }}></div>
+    ) : null,
+    duration: duration,
+    style: {
+      width: "440px",
+      maxHeight: "600px",
+      overflowY: "auto"
+    }
   });
 };
 
--- src/utils/format/error.ts
+++ src/utils/format/error.ts
@@ -1,5 +1,12 @@
 import ApiError from "../../components/ApiError";
 
+export type ApiNotificationError = {
+  error_title: string;
+  error_type: "error" | "warning";
+  error_field?: string;
+  error_values?: string[];
+};
+
 export function parseErrorText(errors: any, t: any) {
   if (typeof errors === "string") {
     return t("ERROR_500");
@@ -27,4 +34,27 @@ export function parseErrorText(errors: any, t: any) {
 export function showApiErrors(errors: any, t: any) {
   const msg = parseErrorText(errors, t);
   ApiError.showError(msg);
+}
+
+export function showApiNotifications(errors: ApiNotificationError[], t: any, duration?: number) {
+  if (!errors?.length) return;
+  errors.forEach((error: ApiNotificationError) => {
+    const { error_title, error_values, error_field, error_type } = error;
+    const title: string = t(error_title, { column_name: error_field || "" });
+
+    const description: string = error_values
+      ?.map((value: string) => {
+        return "<li>" + t(value) + "</li>";
+      })
+      .join("");
+
+    ApiError.showNotification(
+      title,
+      description
+        ? `<ul style='padding-left: 0; margin-bottom: 0;'>${description}</ul>`
+        : null,
+      error_type || "error",
+      duration
+    );
+  });
 }
