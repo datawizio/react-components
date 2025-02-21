b5b42f7eed10b53d79a95501f53f2fc51efd87f7
Maksym Kovpak <kovpak.maksym@gmail.com>
2025-02-21 12:25:57 +0200

    Add autocomplete prop for text field

--- src/components/FormBuilder/types.ts
+++ src/components/FormBuilder/types.ts
@@ -64,6 +64,7 @@ export interface FieldDatePickerProps extends FormFieldProps<Dayjs> {
 
 export interface FieldTextProps extends FormFieldProps<string> {
   type?: string;
+  autoComplete?: string;
 }
 
 export interface FieldCheckboxProps extends FormFieldProps<boolean> {}
