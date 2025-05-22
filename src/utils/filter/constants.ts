76fda68e8a708311f95280012696623508cde133
Maksym Kovpak <kovpak.maksym@gmail.com>
2025-05-22 17:14:09 +0300

    Fix ABC/XYZ filter

--- src/utils/filter/constants.ts
+++ src/utils/filter/constants.ts
@@ -11,3 +11,14 @@ export const defaultAbcXyzValues = {
   price_abc: { a: 0, b: 50, c: 100, last: "∞", select: null },
   coefficient_of_variation_abc: { x: 0.4, y: 0.6, z: 10, select: null }
 };
+
+export const abcXyzMetricsOrder = [
+  "turnover_abc",
+  "products_qty_abc",
+  "receipts_qty_abc",
+  "profit_abc",
+  "actual_stock_qty_abc",
+  "actual_stock_values_abc",
+  "price_abc",
+  "coefficient_of_variation_abc"
+];
